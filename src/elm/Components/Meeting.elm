96b9386cc8f2f243deef51d2a32668fc08001cca
Stratos Xakoustos <stratouklos@gmail.com>
2017-05-06 20:56:20 -0400

    Add missing file

---
+++ src/elm/Components/Meeting.elm
@@ -0,0 +1,27 @@
+module Components.Meeting exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import String
+
+type Phase = Starting | Voting | Discussing
+
+message : Phase -> Html a
+message phase =
+  case phase of
+    Starting    -> div [class "alert alert-info"] [text "Pick a topic and discuss."]
+    Voting      -> div [class "alert alert-success"] [text "That was great! Want more of the same?"]
+    Discussing  -> div [class "alert alert-warning"] [text "Remember to stay on topic!"]
+
+startNewTopicBtn = button [class "lean-btn-blue "][text "Start on a new topic"]
+timeToWrapUpBtn  = button [class "lean-btn-yellow"][text "Hmm, just a bit more..."]
+moreTimeBtn      = button [class "lean-btn-green"][text "Yes, this is fascinating!"]
+
+buttonsDiv = div [class "buttons row"]
+
+controlls : Phase -> Html div
+controlls phase =
+  case phase of
+    Starting    -> buttonsDiv [ startNewTopicBtn ]
+    Voting      -> buttonsDiv [ startNewTopicBtn, moreTimeBtn, timeToWrapUpBtn]
+    Discussing  -> buttonsDiv [ startNewTopicBtn ]
